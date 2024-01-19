bd459f8a714410528db73ea235254a6ff248b0a2
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-01-20 00:05:58 +0530

    feat: display language charts

--- components/LanguageChart.tsx
+++ components/LanguageChart.tsx
@@ -1,20 +1,34 @@
-// components/LanguageChart.tsx
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
-const LanguageChart = ({
-  languageData,
-}: {
-  languageData: Record<string, number>;
-}) => {
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const LanguageChart = ({ languageData }: { languageData: any }) => {
   const chartData = {
     labels: Object.keys(languageData),
     datasets: [
       {
         label: "Programming Languages",
         data: Object.values(languageData),
-        backgroundColor: ["red", "blue", "green", "yellow", "purple", "orange"],
-        borderWidth: 1,
+        backgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+          "#9966FF",
+          "#C9CBCF",
+          // Add more colors as needed
+        ],
+        hoverBackgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+          "#9966FF",
+          "#C9CBCF",
+          // Add more hover colors as needed
+        ],
       },
     ],
   };
