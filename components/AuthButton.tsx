f8645ef4ad71d85c9dc8c4f84a56574d787f23c2
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-04-13 00:00:40 +0530

    fix: minor styling changes

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -30,12 +30,20 @@ export default function AuthButton({ user }: AuthButtonProps) {
   };
 
   return user ? (
-    <Button variant="outline" onClick={handleLogout}>
+    <Button
+      variant="outline"
+      className="inline-flex h-9 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+      onClick={handleLogout}
+    >
       <GitHubLogoIcon className="mr-2 h-4 w-4" /> Logout
     </Button>
   ) : (
-    <Button variant="outline" onClick={handleLoginWithGithub}>
-      <GitHubLogoIcon className="mr-2 h-4 w-4" /> Login with GitHub
+    <Button
+      variant="outline"
+      className="inline-flex h-9 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+      onClick={handleLoginWithGithub}
+    >
+      <GitHubLogoIcon className="mr-2 h-4 w-4" /> Login
     </Button>
   );
 }
