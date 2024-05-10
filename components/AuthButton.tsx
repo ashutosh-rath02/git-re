55e9a124826e093421a93d86443d182a4e5afad3
Tanishq Mishra <mishratanishq619@gmail.com>
2024-05-11 00:58:27 +0530

    Fixed_loginBtnHover_#42

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -7,45 +7,45 @@ import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 
 type AuthButtonProps = {
-  user: User | null;
-  height?: number;
-  width?: number;
+	user: User | null;
+	height?: number;
+	width?: number;
 };
 
 export default function AuthButton({ user, height, width }: AuthButtonProps) {
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleLoginWithGithub = () => {
-    const supabase = supabaseBrowser();
-    supabase.auth.signInWithOAuth({
-      provider: "github",
-      options: {
-        redirectTo: location.origin + "/auth/callback",
-      },
-    });
-  };
+	const handleLoginWithGithub = () => {
+		const supabase = supabaseBrowser();
+		supabase.auth.signInWithOAuth({
+			provider: "github",
+			options: {
+				redirectTo: location.origin + "/auth/callback",
+			},
+		});
+	};
 
-  const handleLogout = async () => {
-    const supabase = supabaseBrowser();
-    await supabase.auth.signOut();
-    router.refresh();
-  };
+	const handleLogout = async () => {
+		const supabase = supabaseBrowser();
+		await supabase.auth.signOut();
+		router.refresh();
+	};
 
-  return user ? (
-    <Button
-      variant="outline"
-      className="inline-flex h-10 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-12 font-medium text-white text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-      onClick={handleLogout}
-    >
-      <GitHubLogoIcon className="mr-2 size-5" /> Logout
-    </Button>
-  ) : (
-    <Button
-      variant="outline"
-      className="inline-flex h-11 animate-shimmer items-center justify-center rounded-md border-2 border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-12 font-medium  text-white text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-      onClick={handleLoginWithGithub}
-    >
-      <GitHubLogoIcon className="mr-2 size-5" /> Login
-    </Button>
-  );
+	return user ? (
+		<Button
+			variant="outline"
+			className="inline-flex h-11 animate-shimmer items-center justify-center rounded-md border-2 border-slate-800 bg-[linear-gradient(110deg,#f7fcff,45%,#ddddff,55%,#f7fcff)]  dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-12 font-medium  text-slate-900 dark:text-white text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+			onClick={handleLogout}
+		>
+			<GitHubLogoIcon className="mr-2 size-5" /> Logout
+		</Button>
+	) : (
+		<Button
+			variant="outline"
+			className="inline-flex h-11 animate-shimmer items-center justify-center rounded-md border-2 border-slate-800 bg-[linear-gradient(110deg,#f7fcff,45%,#ddddff,55%,#f7fcff)]  dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-12 font-medium  text-slate-900 dark:text-white text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+			onClick={handleLoginWithGithub}
+		>
+			<GitHubLogoIcon className="mr-2 size-5" /> Login
+		</Button>
+	);
 }
