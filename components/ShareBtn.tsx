e915f652422762fe8587b89dc0fc2d938e77aa0b
ratishjain <ratishjain6@gmail.com>
2024-05-11 15:56:39 +0530

    sharing

--- components/ShareBtn.tsx
+++ components/ShareBtn.tsx
@@ -34,19 +34,19 @@ const ShareBtn = ({ username }: { username: string }) => {
           <DialogDescription className="flex gap-3">
             <LinkedinShareButton
               className="rounded-full"
-              url={`${window.origin}/username/${username}`}
+              url={`${window.origin}/resume/${username}`}
             >
               <LinkedinIcon className="rounded-full" size={40} />
             </LinkedinShareButton>
             <WhatsappShareButton
               className="rounded-full"
-              url={`${window.origin}/username/${username}`}
+              url={`${window.origin}/resume/${username}`}
             >
               <WhatsappIcon className="rounded-full" size={40} />
             </WhatsappShareButton>
             <TwitterShareButton
               className="rounded-full"
-              url={`${window.origin}/username/${username}`}
+              url={`${window.origin}/resume/${username}`}
             >
               <TwitterIcon className="rounded-full" size={40} />
             </TwitterShareButton>
