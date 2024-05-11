850ead9e4cb2670c9e4996c27e1e5d41ad325a5f
Rishabh Bijalwan <rishabhbijalwan19@gmail.com>
2024-05-11 18:10:21 +0530

    Updated the Login btn

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -34,7 +34,7 @@ export default function AuthButton({ user, height, width }: AuthButtonProps) {
   return user ? (
     <Button
       variant="outline"
-      className="inline-flex items-center justify-center rounded-md px-12 font-medium text-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-50 transition-all h-10 border border-slate-800 bg-gradient-to-r from-gray-300 to-gray-400 hover:from-gray-400 hover:to-gray-500 text-black focus:ring-gray-400"
+      className="inline-flex h-11 animate-shimmer items-center justify-center rounded-md border-2 border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-12 font-medium  text-white text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 hover:text-primary  "
       onClick={handleLogout}
     >
       <GitHubLogoIcon className="mr-2 size-5" /> Logout
@@ -42,7 +42,7 @@ export default function AuthButton({ user, height, width }: AuthButtonProps) {
   ) : (
     <Button
       variant="outline"
-      className="inline-flex items-center justify-center rounded-md px-12 font-medium text-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-50 transition-all h-11 border-2 border-slate-800 bg-gradient-to-r from-gray-300 to-gray-400 hover:from-gray-400 hover:to-gray-500 text-black focus:ring-gray-400"
+      className="inline-flex h-11 animate-shimmer items-center justify-center rounded-md border-2 border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-12 font-medium  text-white text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 hover:text-primary"
       onClick={handleLoginWithGithub}
     >
       <GitHubLogoIcon className="mr-2 size-5" /> Login
