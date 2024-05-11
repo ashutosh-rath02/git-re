c3b6c9f7deac03fc2114998e977e5f3187cdb748
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-05-11 13:49:57 +0530

    successfully added husky workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,36 @@
 module.exports = {
+  root: true,
+  ignorePatterns: ["node_modules/**", "dist/**"],
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "google", "eslint:recommended"],
-  plugins: ["react", "@typescript-eslint"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "next",
+    "prettier",
+  ],
+  file: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeautures: {
+    ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-
-  rules: {},
-
-  settings: {
-    react: {
-      version: "latest",
-    },
+  rules: {
+    "react/no-direct-mutation-state": [
+      "error", // Keep the default as error
+      {
+        ignoreCallbacks: true, // Allow mutation within callbacks (optional)
+        mutators: ["this.setState"], // Allow mutation using this.setState (optional)
+      },
+    ],
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
   },
 };
+// eslint-disable-next-line no-undef
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginReactConfig,
 ];
