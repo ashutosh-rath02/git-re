57f8a4cc9d44aa3332420b0d963442360f2612e2
Nilanchal <nilanchalpanda2003@gmail.com>
2024-05-12 18:55:47 +0530

    Testimonail data added from Product Hunt

--- components/UserTestimonials.tsx
+++ components/UserTestimonials.tsx
@@ -6,46 +6,37 @@ const UserTestimonails = () => {
   const testimonials = [
     {
       quote:
-        "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-      name: "Charles Dickens",
-      title: "A Tale of Two Cities",
+        "Interesting ! Is there a way to display public repositories from organizations made by the user ?",
+      name: "Yannick",
+      title: "Full-Stack Developer",
     },
     {
       quote:
-        "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-      name: "William Shakespeare",
-      title: "Hamlet",
+        "The idea is very cool :) Would love to be able to export the result as a PDF I can share",
+      name: "Matt El Mouktafi",
+      title: "Founder @Achiev",
     },
     {
-      quote: "All that we see or seem is but a dream within a dream.",
-      name: "Edgar Allan Poe",
-      title: "A Dream Within a Dream",
-    },
-    {
-      quote:
-        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-      name: "Jane Austen",
-      title: "Pride and Prejudice",
-    },
-    {
-      quote:
-        "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-      name: "Herman Melville",
-      title: "Moby-Dick",
+      quote: "Looks nice. Good luck to you today",
+      name: "Garen Orchyan",
+      title: "Building Asyncinterview",
     },
   ];
 
   return (
     <div className="h-[40rem] w-full relative flex flex-col items-center justify-center overflow-hidden">
       <h2 className="text-3xl font-bold text-center mb-8 z-10">
-        Hear from our Users
+        Hear our Users about{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-br gradient-radial from-blue-500 to-blue-900">
+          git-re
+        </span>
       </h2>
       <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-6xl">
           <InfiniteMovingCards
             items={testimonials}
             direction="right"
-            speed="slow"
+            speed="fast"
           />
         </div>
       </div>
