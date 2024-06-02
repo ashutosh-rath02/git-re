21981d8e5e7677fc6c52dc12c98c94254452cbe7
Bheemisetty Sharmila <102676949+sharmi4590@users.noreply.github.com>
2024-06-03 03:34:17 +0530

    Update Hamburger.tsx

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -25,12 +25,12 @@ const Hamburger = () => {
         <div className="absolute z-50 w-fit h-fit p-3  top-6 -right-4 rounded-md flex md:hidden flex-col gap-2 ">
           <div className="w-full h-fit p-2 border rounded-md">
             <Link
-              href="https://github.com/ashutosh-rath02/git-re"
+              href={"/"}
               target="_blank"
               rel="noopener noreferrer"
               className="text-navbarSecondary hover:text-primary font-semibold transition flex md:hidden  dark:text-white"
             >
-              Opensource
+             Home
             </Link>
           </div>
 
