0d8cfac55a6ac51f3fe22c20ee2771b48282c7b3
naishub <patelnaisargi74@gmail.com>
2024-06-17 20:22:26 +0530

    webbg

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
   return (
     <main className="flex flex-col my-[8%] items-center justify-center p-4 lg:px-24">
       <div className="relative mb-[6%] h-full flex flex-col max-w-4xl gap-8 place-items-center">
-        <div className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20 pointer-events-none">
+        <div className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-30 pointer-events-none">
           <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
           <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
         </div>
