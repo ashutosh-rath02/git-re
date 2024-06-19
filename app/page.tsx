c3d261c90d0c733c57d7e16db8a95ec6da0b3507
Ashutosh Rath <ashutosh123rath@gmail.com>
2024-06-19 21:07:18 +0530

    Revert "Website Background (#300)"
    This reverts commit fcb55f3e9c48a303092a35dccf984c9304f1af3e.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import "./globals.css";
 import { useSearchParams } from "next/navigation";
 import { toast } from "@/components/ui/use-toast";
 import Lenis from "@studio-freight/lenis";
-import { HeroHighlight } from "@/components/ui/hero-highlight";
 export default function Home() {
   const supabase = supabaseBrowser();
   const searchParams = useSearchParams();
@@ -40,63 +39,58 @@ export default function Home() {
     requestAnimationFrame(raf);
   }, []);
   return (
-    <main className="flex flex-col items-center justify-center p-4 lg:px-24">
-      <HeroHighlight>
-        <div className="relative mt-[5%] mb-[2%] h-full flex flex-col max-w-4xl gap-8 place-items-center">
-          <div className="absolute mt-[8%] inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-30 pointer-events-none">
-            <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-900"></div>
-            <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
-          </div>
-
-          <div className="flex flex-col items-center justify-center  space-y-4 z-10 text-center">
-            <h1 className="text-5xl md:text-7xl lg:text-8xl font-black">
-              <span className="text-transparent bg-clip-text text-blue-400 dark:text-blue-600">
-                git-re
-              </span>
-            </h1>
-            <p className="text-xl md:text-2xl lg:text-4xl font-semibold">
-              Elevate your&nbsp;
-              <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-blue-700">
-                GitHub
-              </span>
-              &nbsp;to a dynamic&nbsp;
-              <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-blue-700">
-                resume
-              </span>
-              &nbsp;effortlessly.
-            </p>
-          </div>
-
-          <div
-            className={
-              data.user
-                ? "flex flex-col items-center justify-center space-y-7"
-                : "flex flex-row space-x-4 items-center"
-            }
-          >
-            {data.user ? (
-              <Form />
-            ) : (
-              <AuthButton user={data.user} className="p-6" />
-            )}
-            <div>
-              <a
-                href="https://www.producthunt.com/posts/git-re?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-git&#0045;re"
-                target="_blank"
-              >
-                <img
-                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=438558&theme=light"
-                  alt="git&#0045;re - Elevate&#0032;your&#0032;GitHub&#0032;to&#0032;a&#0032;dynamic&#0032;resume&#0032;effortlessly | Product Hunt"
-                  style={{ height: "58px" }}
-                  height="58"
-                />
-              </a>
-            </div>
+    <main className="flex flex-col my-[8%] items-center justify-center p-4 lg:px-24">
+      <div className="relative mb-[6%] h-full flex flex-col max-w-4xl gap-8 place-items-center">
+        <div className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20 pointer-events-none">
+          <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+          <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-4 z-10 text-center">
+          <h1 className="text-5xl md:text-7xl lg:text-8xl font-black">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br gradient-radial from-blue-500 to-blue-900">
+              git-re
+            </span>
+          </h1>
+          <p className="text-xl md:text-2xl lg:text-4xl font-semibold">
+            Elevate your&nbsp;
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-blue-700">
+              GitHub
+            </span>
+            &nbsp;to a dynamic&nbsp;
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-blue-700">
+              resume
+            </span>
+            &nbsp;effortlessly.
+          </p>
+        </div>
+        <div
+          className={
+            data.user
+              ? "flex flex-col items-center justify-center space-y-7"
+              : "flex flex-row space-x-4 items-center"
+          }
+        >
+          {data.user ? (
+            <Form />
+          ) : (
+            <AuthButton user={data.user} className="p-6" />
+          )}
+          <div>
+            <a
+              href="https://www.producthunt.com/posts/git-re?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-git&#0045;re"
+              target="_blank"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=438558&theme=light"
+                alt="git&#0045;re - Elevate&#0032;your&#0032;GitHub&#0032;to&#0032;a&#0032;dynamic&#0032;resume&#0032;effortlessly | Product Hunt"
+                style={{ height: "58px" }}
+                height="58"
+              />
+            </a>
           </div>
         </div>
-      </HeroHighlight>
-
-      <div className="h-[22rem] w-full flex flex-col items-center justify-center">
+      </div>
+      <div className="h-[30rem] w-full flex flex-col items-center justify-center">
         <div className="py-4">
           <p
             className="text-4xl font-bold text-center mt-4"
