725f7993110c891f2681e5d9e3ac265e94b12990
Dhairya Shah <dtshah24022006@gmail.com>
2024-07-02 00:04:26 +0530

    fixed: The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead. (#343)
    * Update next.config.js
    
    * Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost",
-      "res.cloudinary.com",
-      "avatars.githubusercontent.com",
-      "github-readme-stats.vercel.app",
-      "ghchart.rshah.org",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/u/*',
+      },
     ],
   },
 };
