67246370395f76abfb7af805aea6ee1928fbb5d2
Dhairya  Shah <dtshah24022006@gmail.com>
2024-07-01 21:57:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,24 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'github-readme-stats.vercel.app',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ghchart.rshah.org',
+        pathname: '/u/*',
       },
     ],
   },
