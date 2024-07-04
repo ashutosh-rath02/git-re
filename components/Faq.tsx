0949d17e69faa98933ae8a4f4f8c4f62f218868c
Ashutosh Rath <ashutosh123rath@gmail.com>
2024-07-04 23:34:10 +0530

    fix: font-size

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -10,7 +10,7 @@ import faqs from "../config/content/faqs";
 const Faq = () => {
   return (
     <>
-      <h1 className="text-center text-5xl mb-10 text-blue-600">FAQs</h1>
+      <h1 className="text-center text-3xl font-bold md:text-4xl mb-10 ">FAQs</h1>
       <div style={{ width: "600px" }}>
         <Accordion type="single" collapsible className="w-full">
           {faqs.map((faq, index) => (
