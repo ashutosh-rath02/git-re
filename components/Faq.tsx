c3dc5c4b593b57c5f9a3a87a80c8388d230f672e
Komal Agarwal <122633300+komal-agarwal5@users.noreply.github.com>
2024-07-04 23:37:47 +0530

    feat: add FAQ section (#359)
    * testimonial responsive
    
    * faq list added
    
    * faq section
    
    * corrections
    
    * corrections pt2
    
    * fix: font-size
    
    ---------
    
    Co-authored-by: Ashutosh Rath <ashutosh123rath@gmail.com>

---
+++ components/Faq.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { use, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import faqs from "../config/content/faqs";
+const Faq = () => {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold md:text-4xl mb-10 ">FAQs</h1>
+      <div style={{ width: "600px" }}>
+        <Accordion type="single" collapsible className="w-full">
+          {faqs.map((faq, index) => (
+            <AccordionItem key={index} value={`item-${index}`}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </>
+  );
+};
+
+export default Faq;
