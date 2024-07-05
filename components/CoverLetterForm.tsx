75febaf3cf772e847e98a0bd8266941ac9ccb008
Sid-80 <siddharthbramhecha@gmail.com>
2024-07-05 22:06:03 +0530

    feat: Multi-Step Form

---
+++ components/CoverLetterForm.tsx
@@ -0,0 +1,262 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "./ui/textarea";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { IconArrowBigRight } from "@tabler/icons-react";
+import { SetStateAction } from "react";
+
+const formSchema = z.object({
+  jobDescription: z
+    .string()
+    .min(10, { message: "Detailed job description required!!" })
+    .max(1000),
+  project: z.string().min(10, { message: "Project required!!" }).max(1000),
+  skills: z.string().min(10, { message: "Skills required!!" }).max(1000),
+  experience: z
+    .string()
+    .min(10, { message: "Experinece required!!" })
+    .max(1000),
+});
+
+type Props = {
+  isJobDescription: boolean;
+  isResumeDetails: boolean;
+  isSubmit: boolean;
+  setIsJobDescription: React.Dispatch<SetStateAction<boolean>>;
+  setIsResumeDetails: React.Dispatch<SetStateAction<boolean>>;
+  setIsSubmit: React.Dispatch<SetStateAction<boolean>>;
+};
+
+export default function CoverLetterForm({
+  isJobDescription,
+  isResumeDetails,
+  isSubmit,
+  setIsJobDescription,
+  setIsSubmit,
+  setIsResumeDetails,
+}: Props) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      jobDescription: "",
+      project: "",
+      skills: "",
+      experience: "",
+    },
+  });
+
+  async function handleNextClick(firstForm: boolean, secForm: boolean) {
+    if (firstForm) {
+      const isValid = await form.trigger("jobDescription");
+      if (isValid) {
+        setIsJobDescription(false);
+        setIsResumeDetails(true);
+      } else {
+        setIsJobDescription(true);
+      }
+    } else {
+      const isValid = await form.trigger(["project", "skills", "experience"]);
+      if (isValid) {
+        setIsResumeDetails(false);
+        setIsSubmit(true);
+      } else {
+        setIsResumeDetails(true);
+      }
+    }
+  }
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 w-[80%] h-full p-2 md:p-10"
+      >
+        {isJobDescription && (
+          <>
+            <FormField
+              control={form.control}
+              name="jobDescription"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[400px]"
+                      placeholder="Detailed Job description with requirements..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              variant={"secondary"}
+              onClick={() => handleNextClick(true, false)}
+              className="flex gap-1"
+              type="button"
+            >
+              Next <ArrowRightIcon className="w-5 h-5" />{" "}
+            </Button>
+          </>
+        )}
+        {isResumeDetails && (
+          <>
+            <FormField
+              control={form.control}
+              name="skills"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Projects</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[100px]"
+                      placeholder="Skills..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="project"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Projects</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[300px]"
+                      placeholder="Projects..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="experience"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Experience</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[400px]"
+                      placeholder="Experience..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              variant={"secondary"}
+              onClick={() => handleNextClick(false, true)}
+              className="flex gap-1"
+              type="button"
+            >
+              Next <ArrowRightIcon className="w-5 h-5" />{" "}
+            </Button>
+          </>
+        )}
+        {isSubmit && (
+          <>
+            <FormField
+              control={form.control}
+              name="jobDescription"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[100px]"
+                      placeholder="Job Description..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="skills"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Projects</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[100px]"
+                      placeholder="Skills..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="project"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Projects</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[300px]"
+                      placeholder="Projects..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="experience"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Experience</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="h-[400px]"
+                      placeholder="Experience..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="flex gap-1" type="submit">
+              Submit
+            </Button>
+          </>
+        )}
+      </form>
+    </Form>
+  );
+}
---
+++ components/cover-letter.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import CoverLetterForm from "./CoverLetterForm";
+
+type Props = {
+  user: User | null;
+};
+
+export default function CoverLetter({ user }: Props) {
+  const router = useRouter();
+  const [isJobDescription, setIsJobDescription] = useState(true);
+  const [isResume, setIsResume] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  if (user === null) {
+    router.push("/");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-full p-2">
+        <div className="flex w-full items-center justify-center">
+          <div className="border-[#B1B2FF] w-[10%] border-0 border-b-2" />
+          <Button
+            className={`${
+              !isJobDescription ? "bg-[#051129] border-gray-400 border" : ""
+            }`}
+            variant={"newDefault"}
+          >
+            1.Job Description
+          </Button>
+          <div className="border-[#B1B2FF] w-[10%] border-0 border-b-2" />
+          <Button
+            className={`${
+              !isResume ? "bg-[#051129] border-gray-400 border" : ""
+            }`}
+            variant={"newDefault"}
+          >
+            2.Resume
+          </Button>
+          <div className="border-[#B1B2FF] w-[10%] border-0 border-b-2" />
+          <Button
+            className={`${
+              !isSubmit ? "bg-[#051129] border-gray-400 border" : ""
+            }`}
+            variant={"newDefault"}
+          >
+            3.Submit
+          </Button>
+          <div className="border-[#B1B2FF] w-[10%] border-0 border-b-2" />
+        </div>
+
+        <div className="flex flex-1 items-center justify-center">
+          <CoverLetterForm
+            isJobDescription={isJobDescription}
+            isResumeDetails={isResume}
+            isSubmit={isSubmit}
+            setIsResumeDetails={setIsResume}
+            setIsJobDescription={setIsJobDescription}
+            setIsSubmit={setIsSubmit}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
