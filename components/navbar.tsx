bf53c73481f14e44f229379ba9f4728d667206fc
shahbaz <76616023+Shahbaz898414@users.noreply.github.com>
2024-07-08 01:55:22 +0530

    Fix Testimonial link behavior on Leaderboard and Compare pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,4 @@
-"use client";  // Mark this component as a Client Component
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ModeToggle } from "./shared/ToggleBg";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { IoMdGitNetwork } from "react-icons/io";
@@ -8,23 +6,13 @@ import Link from "next/link";
 
 import AuthButton from "./AuthButton";
 import Hamburger from "./Hamburger";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { usePathname } from "next/navigation";  // Import the usePathname hook
+import { supabaseServer } from "@/utils/supabase/server";
 
-export default function Navbar() {
+export default async function Navbar() {
   const repositoryUrl = "https://github.com/ashutosh-rath02/git-re";
-  const supabase = createClientComponentClient();  // Use client-side Supabase client
-  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const getUser = async () => {
-      const { data } = await supabase.auth.getUser();
-      setUser(data.user);
-    };
-    getUser();
-  }, [supabase]);
-
-  const currentPath = usePathname();  // Use the usePathname hook to get the current path
+  const supabase = supabaseServer();
+  const { data } = await supabase.auth.getUser();
 
   return (
     <nav className="max-w-screen-2xl mx-auto sm:px-20 px-3 m-2 p-4 w-full flex items-center justify-between">
@@ -45,23 +33,13 @@ export default function Navbar() {
         >
           Home
         </Link>
-        {currentPath === "/leaderboard" ? (
-          <Link
-            href={"/#userTestimonials"}
-            className="text-navbarSecondary hover:text-primary font-semibold transition md:block hidden dark:text-white"
-            style={{ scrollBehavior: "smooth" }}
-          >
-            Testimonial
-          </Link>
-        ) : (
-          <a
-            href="#userTestimonials"
-            className="text-navbarSecondary hover:text-primary font-semibold transition md:block hidden dark:text-white"
-            style={{ scrollBehavior: "smooth" }}
-          >
-            Testimonial
-          </a>
-        )}
+        <Link
+          href="/#userTestimonials"
+          className="text-navbarSecondary hover:text-primary font-semibold transition md:block hidden dark:text-white"
+          style={{ scrollBehavior: "smooth" }}
+        >
+          Testimonial
+        </Link>
         <Link
           href={"/leaderboard"}
           rel="noopener noreferrer"
@@ -77,7 +55,7 @@ export default function Navbar() {
           Compare
         </Link>
 
-        <AuthButton user={user} />
+        <AuthButton user={data.user} />
 
         <Hamburger />
       </div>
