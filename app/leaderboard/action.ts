e1c7990ac3d089003000ba382ada5f2eb0cb203d
Komal Agarwal <122633300+komal-agarwal5@users.noreply.github.com>
2024-07-18 22:06:17 +0530

    Searchbar added (#376)
    * testimonial responsive
    
    * search bar added
    
    * Update page.tsx search bar leaderboard
    
    * corrections
    
    * Update page.tsx
    
    * Update page.tsx
    
    * search from db
    
    ---------
    
    Co-authored-by: Ashutosh Rath <ashutosh123rath@gmail.com>

--- app/leaderboard/action.ts
+++ app/leaderboard/action.ts
@@ -5,20 +5,30 @@ const supabase = supabaseServer();
 
 type LeaderboardProp = {
   page?: number;
+  searchQuery?: string;
 };
 
-export const getLeaderboard = async ({ page = 1 }: LeaderboardProp) => {
+export const getLeaderboard = async ({
+  page = 1,
+  searchQuery = "",
+}: LeaderboardProp) => {
   try {
     const itemsPerPage = 20;
     const from = (page - 1) * itemsPerPage;
     const to = from + itemsPerPage - 1;
 
-    const { data, error, count } = await supabase
+    let query = supabase
       .from("recent_users")
       .select("*", { count: "exact" })
       .neq("rating", "-0.1")
       .order("rating", { ascending: false })
       .range(from, to);
+
+    if (searchQuery) {
+      query = query.ilike("username", `%${searchQuery}%`);
+    }
+
+    const { data, error, count } = await query;
 
     const maxPages = count ? Math.ceil(count / itemsPerPage) : 0;
 
